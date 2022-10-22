bb37457e051efb9ae5bd5c42848b2d5676b829e7
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2022-10-22 18:01:44 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (99.58%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -2,26 +2,18 @@ class Solution {
     public List<List<Integer>> generate(int R) {
         
         List<List<Integer>> M = new ArrayList<>();
-      List<Integer> obj1 = new ArrayList<>();
+        List<Integer> obj1 = new ArrayList<>();
         obj1.add(1);
          M.add(obj1);
         
          for(int i=1;i<R;i++){
-               List<Integer> obj = new ArrayList<>();
+              List<Integer> obj = new ArrayList<>();
               obj.add(1);
-            System.out.println(obj);
             for(int j=1;j<i;j++){
-                 System.out.println(i+"   "+j);
-                  System.out.println(M.get(i-1));
-                  System.out.println(M.get(i-1).get(j-1));
                   obj.add(M.get(i-1).get(j-1)+M.get(i-1).get(j));
             }
             obj.add(1);
-             System.out.println(obj);
-             System.out.println(M);
-             M.add(obj);
-            System.out.println(M);
-            
+            M.add(obj);
     }
         
         
