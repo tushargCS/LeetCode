da5347eb09d396898ba1ea457b4ac8183085f626
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2023-01-05 09:46:50 +0530

    Time: 20 ms (5.17%), Space: 41 MB (54.01%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,28 @@
+class Solution {
+    List<List<Integer>>obj = new ArrayList<>();
+    public List<List<Integer>> combinationSum3(int k, int n) {
+     
+        
+        fun(new ArrayList(),n,k,1);
+        return obj;
+        
+    }
+    
+    public void fun(List<Integer>li,int n,int k,int j){
+        if( n==0 && li.size()==k){
+            List<Integer>o = new ArrayList<>(li);
+            Collections.sort(o);
+            if(!obj.contains(o)){ obj.add(o);}
+            return;}
+        if(li.size()==k){return;}
+        for(int i=j;i<=9;i++){
+            if(!li.contains(i)){li.add(i);}
+            else{continue;}
+            fun(li,n-i,k,j+1);
+            int index = li.size()-1;
+            if(index!=-1) {li.remove(index);}
+        }
+        
+    }
+    
+}
