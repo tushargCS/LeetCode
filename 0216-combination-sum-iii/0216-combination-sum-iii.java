ef7b3e1f799b95bddbbc4cd7ca989da10e4822a4
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2023-01-05 10:01:05 +0530

    Time: 1 ms (61.46%), Space: 41.7 MB (37.08%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -11,18 +11,65 @@ class Solution {
     public void fun(List<Integer>li,int n,int k,int j){
         if( n==0 && li.size()==k){
             List<Integer>o = new ArrayList<>(li);
-            Collections.sort(o);
-            if(!obj.contains(o)){ obj.add(o);}
+            obj.add(o);
             return;}
         if(li.size()==k){return;}
         for(int i=j;i<=9;i++){
-            if(!li.contains(i)){li.add(i);}
-            else{continue;}
-            fun(li,n-i,k,j+1);
+            li.add(i);
+            fun(li,n-i,k,i+1);
             int index = li.size()-1;
-            if(index!=-1) {li.remove(index);}
+            li.remove(index);
         }
         
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     List<List<Integer>>obj = new ArrayList<>();
+//     public List<List<Integer>> combinationSum3(int k, int n) {
+     
+        
+//         fun(new ArrayList(),n,k,1);
+//         return obj;
+        
+//     }
+    
+//     public void fun(List<Integer>li,int n,int k,int j){
+//         if( n==0 && li.size()==k){
+//             List<Integer>o = new ArrayList<>(li);
+//             Collections.sort(o);
+//             if(!obj.contains(o)){ obj.add(o);}
+//             return;}
+//         if(li.size()==k){return;}
+//         for(int i=j;i<=9;i++){
+//             if(!li.contains(i)){li.add(i);}
+//             else{continue;}
+//             fun(li,n-i,k,j+1);
+//             int index = li.size()-1;
+//             if(index!=-1) {li.remove(index);}
+//         }
+        
+//     }
+    
+// }
