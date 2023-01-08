c083c9661eef00331dc74a1e0b1bc6f007f53df5
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2023-01-08 22:45:12 +0530

    Time: 1 ms (100.00%), Space: 62.7 MB (67.95%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,48 +1,23 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         
-        
-        
-        int start=-1;
-        int i=0;
-        int temp=0;
-        int j=0;
-        int prev=-1;
-        int ps=-1;
-        int g=0; 
-        
-          for(int v=0;v<gas.length;v++){
-            g+=gas[v]-cost[v];
+        int t1=0;
+        for(int i=0;i<cost.length;i++){
+            t1+=gas[i]-cost[i];
         }
-        if(g<0){return -1;}
+        if(t1<0){return -1;}
         
-        while(prev!=i){
-            
-            if((gas[i]+temp)>=cost[i]){
-                start=i;
-                temp = (gas[i]+temp)-cost[i];
-                int s = i;
-                if(ps==s){return -1;}
-                else{ps=s;}
-                i++;
-                i = i%gas.length;
-                while((gas[i]+temp)>=cost[i] && s!=i){
-                    temp = (gas[i]+temp)-cost[i];
-                    i++;
-                    i = i%gas.length;
-                }
-                // if(prev==i){return -1;}
-                // if(prev==-1){prev=i;}
-                if(s==i){return s;}
-                temp=0;
+        
+        int s=0;
+        int total=0;
+        for(int i=0;i<cost.length;i++){
+            total+=(gas[i]-cost[i]);
+            if(total<0){
+                total=0;
+                s=i+1;
             }
-            i++;
-             if(prev==-1 || start==-1){if(i==gas.length)return -1; }
-              i = i%gas.length;
         }
         
-        
-        return -1;
-        
+        return s;
     }
 }
