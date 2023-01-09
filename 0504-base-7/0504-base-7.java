ede24a862006c28a8b68850c95f46e944977e05f
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2023-01-09 22:26:42 +0530

    Time: 10 ms (17.91%), Space: 40.5 MB (63.95%) - LeetHub

---
+++ 0504-base-7/0504-base-7.java
@@ -0,0 +1,64 @@
+class Solution {
+    public String convertToBase7(int num) {
+     
+        if(num==0){return "0";}
+        
+       int d=7;
+	   int n=num;
+	   String s="";
+      
+        int i=10;
+        int t=0;
+        
+        int pos=1;
+        if(n<0){pos=0;}
+         n=Math.abs(n);
+        
+       
+	  
+        
+	  
+	  
+	  
+	  while(i>=0){
+	  
+	   int j=1;
+	  
+	    t=0;
+	 
+	 
+	   if(n%(int)Math.pow(d,i)<n){
+	      t = n%(int)Math.pow(d,i);
+	 
+	    while((j*Math.pow(7,i))<=n){
+	      j++;
+	    } 
+	    j--;
+	    n=t;
+	    s+=j;
+	   
+	   t=0;
+	  }
+	  
+	   else{
+	       s+=0;
+	   }
+	  
+	   i--;
+	  }
+	  
+	  i=0;
+	 
+	  while(s.charAt(i)=='0'){
+	      i++;
+	  }
+	  
+	  s=s.substring(i,s.length());
+	   
+        if(pos==0){s='-'+s;}
+        
+	  return (s);
+	   
+        
+    }
+}
