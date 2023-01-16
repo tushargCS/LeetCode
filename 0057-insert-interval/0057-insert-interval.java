4eb7d7ead3da9c4f10ef1465c3cb499de1b148ad
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2023-01-16 21:01:47 +0530

    Time: 1 ms (99.26%), Space: 44.8 MB (58.02%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,76 +1,44 @@
 class Solution {
     public int[][] insert(int[][] old, int[] newarr) {
-        ArrayList<ArrayList<Integer>>obj = new ArrayList<>();
-        boolean flag = false;
-     
-         if(old.length==0){
-            int r[][]= new int[1][2];
-            r[0][0]=newarr[0];
-            r[0][1]=newarr[1];
-            return r;
-        }
+        ArrayList<int[]>obj = new ArrayList<>();
         
-
-        
-        
-    for(int i=0;i<old.length;i++){
-          if(old[i][0]<newarr[0]){
-              ArrayList<Integer> o  =new ArrayList<>();
-              o.add(old[i][0]);
-              o.add(old[i][1]);
-              obj.add(o);
-          }
-          else if(flag==false){
-              ArrayList<Integer> g  =new ArrayList<>();
-              g.add(newarr[0]);
-              g.add(newarr[1]);
-              obj.add(g);
-               ArrayList<Integer> o  =new ArrayList<>();
-              o.add(old[i][0]);
-              o.add(old[i][1]);
-              obj.add(o);
-              flag=true;
-          }
-          else{
-               ArrayList<Integer> o  =new ArrayList<>();
-              o.add(old[i][0]);
-              o.add(old[i][1]);
-              obj.add(o);
-          }
-      }
-        
-        if(flag==false){
-             ArrayList<Integer> g  =new ArrayList<>();
-              g.add(newarr[0]);
-              g.add(newarr[1]);
-              obj.add(g);
-        }
-        
-          
-        
-        
-        for(int i=0;i<obj.size()-1;i++){
-            if(obj.get(i).get(1)>=obj.get(i+1).get(0) && obj.get(i).get(1)<=obj.get(i+1).get(1) ){
-                obj.get(i).remove(1);
-                obj.get(i).add(obj.get(i+1).get(1));
-                obj.remove(i+1);
-                i--;
+        int i=0;
+        while(i<old.length){
+            if(newarr[0]>old[i][0]){
+                obj.add(old[i]);
+                i++;
             }
-            else if(obj.get(i).get(1)>obj.get(i+1).get(1)){
-                obj.remove(i+1);
-                i--;
+            else{
+                break;
             }
         }
         
-        
-       int r1[][]= new int[obj.size()][2];
-    
-        for(int j=0;j<r1.length;j++){
-            r1[j][0]= (obj.get(j).get(0));
-            r1[j][1]= (obj.get(j).get(1));
+        if(obj.size()==0 || ( obj.get(obj.size()-1)[1] < newarr[0] ) ){
+            obj.add(newarr);
+        }
+        else{
+            int last[] = obj.get(obj.size()-1);
+            last[1] = Math.max(newarr[1],last[1]);
         }
         
-        return r1;
-            
+        
+        while(i<old.length){
+            int last[] =  obj.get(obj.size()-1);
+             if(last[1]<old[i][0]){
+                 obj.add(old[i]);
+             }
+            else{
+                 last[1] = Math.max(last[1],old[i][1]);   
+            }
+            i++;
+        }
+        
+        int r [][] = new int[obj.size()][2];
+        for(i=0;i<obj.size();i++){
+            r[i]=obj.get(i);
+        }
+        
+        return r;
+        
     }
 }
