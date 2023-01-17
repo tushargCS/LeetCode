cc93d12db6628be3fe1a62b094f8f218482145fb
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2023-01-17 17:21:21 +0530

    Time: 16 ms (95.77%), Space: 39.2 MB (68.58%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -11,22 +11,11 @@ public class Solution extends VersionControl {
         int mid=0;
          mid=i+(j-i)/2;
         
-      
-        ArrayList<Integer> obj = new ArrayList<>();
-        
         while(i<=j){
-            System.out.println(mid);
             if(isBadVersion(mid)==false){i=mid+1; }
-            else{obj.add(i);j=mid-1;}
+            else{j=mid-1;}
             mid=i+(j-i)/2;
         }
-      
-        // System.out.println(obj.size());
-        //  int min = obj.get(0);
-        //  for(int x=0;x<obj.size();x++){
-        //      System.out.println(mid);
-        //      min=Math.min(obj.get(x),min);
-        //  }
         
         return (int)mid;
         
