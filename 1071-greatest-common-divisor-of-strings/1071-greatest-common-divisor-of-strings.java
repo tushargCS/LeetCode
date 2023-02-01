bfa9958d2ad8f4235bb76ccfef5de09f311ec19c
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2023-02-01 12:01:42 +0530

    Time: 22 ms (10.83%), Space: 42.5 MB (43.31%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        for(int i=0;i<str2.length();i++){
+            if(str1.contains(str2.substring(i,str2.length()))){
+                boolean b = check(str1,str2,str2.substring(i,str2.length()));
+                if(b==true){return (str2.substring(i,str2.length())); }
+            }
+        }
+        return "";
+    }
+    
+    public boolean check(String str1,String str2,String sub){
+       boolean x = false;
+        boolean y = false;
+        String temp = sub;
+        while(str1.length()>=temp.length()){
+            if(str1.equals(temp)){x=true;break;}
+            temp = temp+sub;
+        }
+        temp = sub; 
+        while(str2.length()>=temp.length()){
+            if(str2.equals(temp)){y=true;break;}
+            temp = temp+sub;
+        }
+       
+        if(x==true && y==true){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+    
+}
