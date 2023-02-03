04e37dbd75d624c1f9a5f6ab072154e787223e38
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2023-02-03 17:32:27 +0530

    Time: 60 ms (10.41%), Space: 49.7 MB (32.12%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,47 +2,31 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         
         List<List<String>> obj = new ArrayList<>();
-        int flag=0;
-        int n = strs.length;
-        for(int i=0;i<n;i++){
-            if(strs[i].equals("")){flag++;}
-        }
-        for(int i=0;i<n;i++){
-            if(strs[i].equals("")){continue;}
-            List<String> temp = new ArrayList<>();
-            temp.add(strs[i]);
-            for(int j=i+1;j<n;j++){
-                boolean b = check(strs[i],strs[j]); 
-                if(b==true){temp.add(strs[j]);strs[j]="";}
-                                  
+        
+        HashMap<HashMap<Character,Integer>,ArrayList<String>> Map = new HashMap<>();
+        
+        for(String s:strs){
+            HashMap<Character,Integer> fre = new HashMap<>();
+            int x = s.length();
+            for(int i=0;i<x;i++){
+                fre.put(s.charAt(i),fre.getOrDefault(s.charAt(i),0)+1);
             }
+            if(Map.containsKey(fre)){
+                ArrayList<String> temp = Map.get(fre);
+                temp.add(s);
+                Map.put(fre,temp);
+            }
+            else{
+                ArrayList<String> temp = new ArrayList<>();
+                temp.add(s);
+                Map.put(fre,temp);
+            }
+        }
+        
+        for(ArrayList<String> temp: Map.values()){
             obj.add(temp);
         }
         
-        List<String> temp1 = new ArrayList<>();
-        int f=0;
-        while(flag>0){
-            f=1;
-            flag--;
-            temp1.add("");
-        }
-       if(f==1) obj.add(temp1);
         return obj;
-    }
-    
-    public boolean check(String s1,String s2){
-        if(s1.length()!=s2.length()){return false;}
-        int n = s1.length();
-        int arr1[] = new int[26];
-        int arr2[] = new int[26];
-        for(int i=0;i<n;i++){
-            arr1[s1.charAt(i)-'a']++;
-             arr2[s2.charAt(i)-'a']++;
-        }
-        
-         for(int i=0;i<26;i++){
-            if(arr1[i]!=arr2[i]){return false;}
-        }
-        return true;
     }
 }
