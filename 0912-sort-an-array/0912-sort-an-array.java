d2e5402e6a8d4dcbd9bf5199798e653dafd696bd
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2023-03-01 20:58:23 +0530

    Time: 38 ms (47.77%), Space: 51.8 MB (84.65%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,48 +1,55 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        merge_sort(nums,0,nums.length-1);
+        quicksort(nums,0,nums.length-1);
         return nums;
     }
     
-    public void merge_sort(int a[],int l,int r){
-        if(l<r){
-            int mid = (l)+(r-l)/2;
-            merge_sort(a,l,mid);
-            merge_sort(a,mid+1,r);
-            merge(a,l,mid,r);
+    public void quicksort(int nums[],int p,int q){
+        if(p<q){
+            int m = (p+q)/2;
+            quicksort(nums,p,m);
+            quicksort(nums,m+1,q);
+            merge(nums,p,m,q);
         }
     }
-    
-    public void merge(int a[],int l,int mid,int r){
-        int n1 = mid-l+1;
-        int n2 = r-mid;
-        int left[] = new int[n1];
-        int right[] = new int[n2];
-        for(int i=0;i<n1;i++){
-            left[i] = a[l+i];
-        }
-        for(int i=0;i<n2;i++){
-            right[i] = a[mid+1+i];
-        }
-        int k = l;
-        int x=0;
-        int y=0;
-        while(x<n1 && y<n2){
-            if(left[x]<=right[y]){
-                a[k++] = left[x++];
-            }
-            else{
-                a[k++] = right[y++];
-            }
-        }
-        while(x<n1){
-             a[k++] = left[x++];
-        }
-        while(y<n2){
-             a[k++] = right[y++];
+   
+    public void merge(int nums[],int p,int m,int q){
+       
+        int n1 = m-p+1;
+        int n2 = q-m;
+        
+        int l[] = new int[n1+1];
+        int r[] = new int[n2+1];
+        
+        for(int i = 0;i<n1;i++){
+            l[i] = nums[p+i];
         }
         
-     
+        for(int i = 0;i<n2;i++){
+            r[i] = nums[m+1+i];
+        }
+        
+        l[n1] = Integer.MAX_VALUE;
+        r[n2] = Integer.MAX_VALUE;
+        
+        int x=0;
+        int y=0;
+        int k = p;
+        while(n1!=0 || n2!=0){
+            if(l[x]<=r[y]){
+                nums[k] = l[x];
+                x++;
+                n1--;
+            }
+            else{
+                nums[k] = r[y];
+                y++;
+                n2--;
+            }
+            k++;
+        }
+    
     }
     
+  
 }
