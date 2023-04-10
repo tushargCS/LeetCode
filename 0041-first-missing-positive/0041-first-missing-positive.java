e6e50da67f084dce22ae6eb19facc91357a2703a
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2023-04-10 09:55:30 +0530

    Time: 2 ms (48.79%), Space: 51.3 MB (24.49%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,17 +1,38 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int max=Integer.MIN_VALUE;
-        HashMap<Integer,Integer> Map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            Map.put(nums[i],Map.getOrDefault(nums[i],0)+1);
-            if(max<nums[i]){max=nums[i];}
+        if(nums.length==0){return 1;}
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int t:nums){
+            if(min>t){
+                min = t;
+            }
+            if(max<t){
+                max=t;
+            }
         }
-        int i=1;
+        if(min>1){return 1;}
         if(max<=0){return 1;}
-        while(i<max){
-            if(!Map.containsKey(i)){return i;}
-            i++;
+        
+        
+        for(int i=0;i<nums.length;){
+            
+            if( nums[i]>nums.length || nums[i]<=0){i++;}
+            else if(nums[nums[i]-1]==nums[i]){
+                i++;
+            } 
+            else{   
+                int t = nums[nums[i]-1];
+                nums[nums[i]-1] = nums[i];
+                nums[i] = t;
+            }
         }
-        return max+1;
+        
+        int j=0;
+        for(j=0;j<nums.length;j++){
+            if(nums[j]!=j+1){return j+1;}
+        }
+        
+        return nums[j-1]+1;
     }
 }
