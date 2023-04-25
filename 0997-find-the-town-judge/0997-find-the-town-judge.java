7d5be5469be69a0053d2ef43a18eb3cf32559b9b
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2023-04-25 13:22:54 +0530

    Time: 26 ms (9.30%), Space: 50.5 MB (44.01%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,31 +1,33 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        
-        if(trust.length==0 && n!=1){return -1;}
-        if(trust.length==0){return 1;}
-        
-        HashSet<Integer>has = new HashSet<>();
+
+        if(trust.length==0){if(n==1){return 1;}
+                           else{return -1;}}
+        HashMap<Integer,HashSet<Integer>> map = new HashMap<>();
         
         for(int i=0;i<trust.length;i++){
-            has.add(trust[i][0]);
+            if(!map.containsKey(trust[i][1])){
+                HashSet<Integer> t = new HashSet<>();
+                t.add(trust[i][0]);
+                map.put(trust[i][1],t);
+            }
+            else{
+                HashSet<Integer> t = map.get(trust[i][1]);
+                t.add(trust[i][0]);
+                map.put(trust[i][1],t);
+            }
+                
         }
         
-        if(has.size()<n-1){return -1;}
+        HashSet<Integer>set = new HashSet<>();
         
-        int judge=0;
-        for(int i=1;i<=n;i++){
-            if(!has.contains(i)){judge=i;break;}
+        for(int []t:trust){
+            set.add(t[0]);
         }
         
-        has.clear();
-       
-        for(int i=0;i<trust.length;i++){
-            if(trust[i][1]==judge){has.add(trust[i][0]);}
+        for(Integer t:map.keySet()){
+            if((map.get(t).size()==n-1) && !set.contains(t)){return t;}
         }
-        
-        if(has.size()==n-1){return judge;}
-        
-        
         
         return -1;
         
