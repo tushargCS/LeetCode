91c1bc0acb4fdd39dbe9b24a97d362a9296c3f36
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2023-05-27 23:26:02 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (90.47%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,11 +1,39 @@
 class Solution {
     public int search(int[] nums, int target) {
+        int t = fun(nums,target);
+        // System.out.println(t);
+        if(nums[t]<=target && nums[nums.length-1]>=target){
+            return bs(nums,target,t,nums.length-1);
+        }
+        else if(nums[0]<=target ){
+            return bs(nums,target,0,t-1);
+        }
+            return -1;
+    }
+    
+    public int fun(int nums[], int t){
         int i=0;
-        while(i<nums.length){
-            if(nums[i]==target){
-                return i;
+        int j=nums.length-1;
+        int mid = (i+j)/2;
+        while(i<j){
+            if(nums[mid]>=nums[0]){i = mid+1;}
+            else{ j=mid;}
+            mid = (i+j)/2;
+        }
+        return i;
+    }
+    
+    public int bs(int nums[],int t,int i,int j){
+        int mid = (i+j)/2;
+        while(i<=j){
+            if(nums[mid]==t){return mid;}
+            else if(nums[mid]<t){
+                i = mid+1;
             }
-            i++;
+            else{
+                j = mid-1;
+            }
+            mid = (i+j)/2;
         }
         return -1;
     }
